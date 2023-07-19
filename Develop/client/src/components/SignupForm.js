e61e5277e77e598c8955084e23f213b644de1358
pvmend <mendoza.v.paul@gmail.com>
2023-07-19 10:03:21 -0700

    useMutations and useQueries

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -3,6 +3,8 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import {useMutation} from '@apollo/client'
+import {ADD_USER} from '../utils/mutations'
 
 const SignupForm = () => {
   // set initial form state
@@ -17,6 +19,7 @@ const SignupForm = () => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  const [addUser, {error}] = useMutation(ADD_USER);
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -28,7 +31,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser({
+        variables : {
+          ...userFormData
+        }
+      })
 
       if (!response.ok) {
         throw new Error('something went wrong!');
